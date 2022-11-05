82f14f66ab46dda0bcfd06d80038e75d02657fed
Sovrano28 <Adeyemiayokanmi47@gmail.com>
2022-11-05 19:34:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ let tens = 00;
 
 let watchSeconds = document.querySelectorAll('.seconds');
 let watchTens = document.querySelectorAll('.tens');
-const startBtn = document.querySelectorAll('.start-btn');
-const stoptBtn = document.querySelectorAll('.stop-btn');
-const resetBtn = document.querySelectorAll('.reset-btn');
+const startBtn = document.querySelectorAll('.btn-start');
+const stoptBtn = document.querySelectorAll('.btn-stop');
+const resetBtn = document.querySelectorAll('.btn-reset');
