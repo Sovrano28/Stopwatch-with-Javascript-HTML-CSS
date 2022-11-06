8e09e38d663955edc88588ba744608da312e6738
Sovrano28 <Adeyemiayokanmi47@gmail.com>
2022-11-06 04:13:31 +0100

    added the reset button

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let tens = 0;
 let stopwatchSeconds = document.querySelector('.seconds');
 let stopwatchTens = document.querySelector('.tens');
 const startBtn = document.querySelector('.btn-start');
-const stoptBtn = document.querySelector('.btn-stop');
+const stopBtn = document.querySelector('.btn-stop');
 const resetBtn = document.querySelector('.btn-reset');
 
 let watchInterval; // creating a variable so as to be able to clear it.
@@ -32,12 +32,23 @@ function startTimer() {
   }
 }
 
-// since we need to hit the start button continually for the tens to increase at all, this function will help us run thefunction startTimer() above continually.
+// since we need to hit the(previous) start button continually for the tens to increase at all, this function will help us run thefunction startTimer() above continually.
 startBtn.addEventListener('click', () => {
   watchInterval = setInterval(startTimer, 10);
-})
+});
 
 // this is the pause button
-stoptBtn.addEventListener('click', () => {
+stopBtn.addEventListener('click', () => {
   clearInterval(watchInterval);
-})
+});
+
+// the reset button
+resetBtn.addEventListener('click', () => {
+  clearInterval(watchInterval);
+
+  tens = '00';
+  seconds = '00';
+
+  stopwatchSeconds.innerHTML = seconds;
+  stopwatchTens.innerHTML = tens;
+});
